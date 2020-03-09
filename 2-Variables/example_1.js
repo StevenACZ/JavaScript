226fd18ef5a74c32aea7ee512a7329f22d7623af
Steven Coaila Zaa <scoailazaa@gmail.com>
2020-03-09 01:39:55 -0500

    Variables

---
+++ 2-Variables/example_1.js
@@ -0,0 +1,15 @@
+////////////////////////
+// Variables
+var nombre = 'Steven';
+var elementos = ['java', 'c++', 'python'];
+var persona = {
+  nombre: 'Steven',
+  edad: 17
+}
+
+// Declarando variable
+var edad;
+
+// Inicializando variable
+edad = 17;
+

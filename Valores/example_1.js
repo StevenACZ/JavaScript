dbcabb0fe8ca90e716ce49758e7cbf72e4da6afc
Steven Coaila Zaa <scoailazaa@gmail.com>
2020-03-09 01:31:14 -0500

    Valores

---
+++ Valores/example_1.js
@@ -0,0 +1,27 @@
+////////////////////////
+// Valores primitivos
+let number = 40;
+let string = 'Steven';
+let boolean = true;
+let indefinido = undefined;
+
+console.log('> Valores primitivos')
+console.log(typeof number, number);
+console.log(typeof string, string);
+console.log(typeof boolean, boolean);
+console.log(typeof indefinido, undefined, '\n');
+
+////////////////////////
+// Valores objetos
+let nulo = null;
+let arr = [1,2,3];
+let obj = {
+  1:1,
+  2:2,
+  3:3
+};
+
+console.log('> Valores objetos')
+console.log(typeof nulo, nulo);
+console.log(typeof arr, arr);
+console.log(typeof obj, obj);

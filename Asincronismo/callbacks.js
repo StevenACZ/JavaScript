143f0c286d0ce43ea73f5c26d9658c51d6123b45
Steven Coaila Zaa <scoailazaa@gmail.com>
2020-03-15 01:08:08 -0500

    callbacks hell

--- Asincronismo/callbacks.js
+++ Asincronismo/callbacks.js
@@ -3,15 +3,27 @@ const PEOPLE_URL = 'people/:id'
 
 const opts = { crossDomain: true }
 
-const onPeopleResponse = function(person) {
-  console.log(`Hola, yo soy ${person.name}`);
-}
-
-function obtenerPersonaje(id) {
+function obtenerPersonaje(id, callback) {
   const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
-  $.get(url, opts, onPeopleResponse)
+  $.get(url, opts, callback)
 }
 
-obtenerPersonaje(1)
-obtenerPersonaje(2)
-obtenerPersonaje(3)
+obtenerPersonaje(1, function(person) {
+  console.log(`Hola, yo soy ${person.name}`);
+  obtenerPersonaje(2, function(person) {
+    console.log(`Hola, yo soy ${person.name}`);
+    obtenerPersonaje(3, function(person) {
+      console.log(`Hola, yo soy ${person.name}`);
+      obtenerPersonaje(4, function(person) {
+        console.log(`Hola, yo soy ${person.name}`);
+        obtenerPersonaje(5, function(person) {
+          console.log(`Hola, yo soy ${person.name}`);
+          obtenerPersonaje(6, function(person) {
+            console.log(`Hola, yo soy ${person.name}`);
+            obtenerPersonaje(7)
+          })
+        })
+      })
+    })
+  })
+})

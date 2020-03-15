ff06c9dfd406da01bc349dddfe3b1d3cd93fb28d
Steven Coaila Zaa <scoailazaa@gmail.com>
2020-03-14 22:40:20 -0500

    Objetos - prototipos

--- Objetos/Prototipos.js
+++ Objetos/Prototipos.js
@@ -1,5 +1,26 @@
-function Persona() {
-  console.log('Me ejecutaron');
+function Persona(nombre, apellido, altura) {
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.edad = 20;
+  this.altura = altura;
 }
 
-let sacha = new Persona();
+Persona.prototype.saludar = function() {
+  console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
+}
+
+Persona.prototype.shortOrTall = function() {
+  if (this.altura >= 1.80) {
+    console.log(`${this.nombre} es alto`);
+  } else {
+    console.log(`${this.nombre} es bajo`);
+  }
+}
+
+let steven = new Persona('Steven', 'Coaila', 1.70);
+let erika = new Persona('Erika', 'Luna', 1.54);
+let arturo = new Persona('Arturo', 'Martines', 1.84);
+
+steven.shortOrTall();
+erika.shortOrTall();
+arturo.shortOrTall();
